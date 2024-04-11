32fea39d70fbeb7902d875ccf99f6744600ef74c
DBin_K <DBinKv1@Gmail.com>
2024-04-11 12:32:53 +0800

    perf👌: 现在性能超高

--- stream.py
+++ stream.py
@@ -1,19 +1,39 @@
 import cv2
+import time
+from threading import Thread
 
-# 打开MJPEG流
-cap = cv2.VideoCapture('http://192.168.50.4:4747/video?640x480')
+class ThreadedCamera(object):
+    def __init__(self, url):
+        self.capture = cv2.VideoCapture(url)
+        self.capture.set(cv2.CAP_PROP_BUFFERSIZE, 2)  # 设置最大缓冲区大小
 
-while True:
-    # 读取一帧
-    ret, frame = cap.read()
+        # 设定帧率为30帧每秒
+        self.FPS = 1 / 60
+        self.FPS_MS = int(self.FPS * 1000)
 
-    # 检查是否成功读取
-    if ret:
-        # 显示帧
-        cv2.imshow('MJPEG Stream', frame)
+        # 启动帧检索线程
+        self.thread = Thread(target=self.update, args=())
+        self.thread.daemon = True
+        self.thread.start()
 
-        # 等待0.1秒（单位：秒）
-        cv2.waitKey(100)
+    def update(self):
+        while True:
+            if self.capture.isOpened():
+                (status, frame) = self.capture.read()
+                if status:
+                    self.frame = frame
+            time.sleep(self.FPS)
 
-# 关闭流
-cap.release()
+    def show_frame(self):
+        cv2.imshow('MJPEG Stream', self.frame)
+        cv2.waitKey(self.FPS_MS)
+
+if __name__ == '__main__':
+    stream_url = 'http://192.168.50.4:4747/video?640x480'
+    threaded_camera = ThreadedCamera(stream_url)
+    
+    while True:
+        try:
+            threaded_camera.show_frame()
+        except AttributeError:
+            pass
