815449d9303f90971430423182d958a86fb1c55f
DBinK <dbinkv1@gmail.com>
2024-04-13 10:04:52 +0800

    fix🐛:

--- stream.py
+++ stream.py
@@ -29,7 +29,7 @@ class ThreadedCamera(object):
                     self.frame = frame.copy()
             time.sleep(self.FPS)
 
-    def process_frame(self, frame):
+    def process_frame_outside(self, frame):
         # 在这里添加您的OpenCV处理代码
         # 例如，可以进行图像处理、对象检测、人脸识别等
 
@@ -44,7 +44,23 @@ class ThreadedCamera(object):
 
         processed_frame = frame
         
-        #processed_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)  # 示例：将帧转换为灰度图像
+        return processed_frame
+    
+    def process_frame_inside(self, frame):
+        # 在这里添加您的OpenCV处理代码
+        # 例如，可以进行图像处理、对象检测、人脸识别等
+
+        contours = preprocess_image(frame)
+        max_perimeter, max_cnt = find_max_perimeter_contour(contours, 500*4) # 最大允许周长(mm)
+
+        if max_cnt is not None:
+            vertices = find_contour_xy(max_cnt, max_perimeter)
+
+        if vertices is not None:
+            frame = draw_contour_and_vertices(frame, vertices, (276/297)) # 外框与内框宽度之比(mm) 靶纸是 (276/297)
+
+        processed_frame = frame
+        
         return processed_frame
 
     def show_frame(self):
@@ -54,7 +70,7 @@ class ThreadedCamera(object):
         #cv2.resizeWindow('Processed Stream', 800, 600)
 
         cv2.imshow('Original MJPEG Stream', self.frame)
-        processed_frame = self.process_frame(self.frame)
+        processed_frame = self.process_frame_outside(self.frame)
         if processed_frame is not None:
             cv2.imshow('Processed Stream', processed_frame)
         cv2.waitKey(self.FPS_MS)
@@ -81,9 +97,9 @@ def generate_frames():
         frame = stream.frame
         if frame is not None:
             try:
-                processed_frame = stream.process_frame(frame)
+                processed_frame = stream.process_frame_outside(frame)
                 # 将处理后的帧编码为JPEG格式
-                ret, jpeg_buffer = cv2.imencode('.jpg', processed_frame)
+                _, jpeg_buffer = cv2.imencode('.jpg', processed_frame)
 
                 # 拼接MJPEG帧并返回
                 yield (b'--frame\r\n'
