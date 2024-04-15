1155bf0215f4e0c0e852e3fdaab7c11c4c973f83
DBin_K <DBinKv1@Gmail.com>
2024-04-16 01:15:22 +0800

    format🥚:

--- test-ctrl.py
+++ test-ctrl.py
@@ -1,8 +1,6 @@
 
-from threading import Thread
 from time import sleep
 import cv2
-import keyboard
 import cam
 
 url = 3
@@ -10,32 +8,7 @@ vcap = cv2.VideoCapture(url)
 
 x, y = 100, 100
 
-# 定义按键监听函数
-def key_listener():
-    def release(event):
-        global track
-        print(event.name)
-        if event.event_type == keyboard.KEY_UP:
-            sleep(0.5)   # 消除抖动
-            if event.name == 'q':
-                print("q is pressed")
-            elif event.name == 'w':
-                print("w is pressed")
-            elif event.name == 'e':
-                print("e is pressed")
-            elif event.name == 't':
-                print("t is pressed")
-                track = 1
-    
-    keyboard.on_release(release)  # 注册按键监听器
-    keyboard.wait()  # 保持监听状态
-
-track = 0
-
 if __name__ == '__main__':
-    # 创建一个线程来监听控制台按键输入
-    #key_thread = Thread(target=key_listener)
-    #key_thread.start()
 
     while True:
         # 读取每一帧图像
@@ -45,17 +18,21 @@ if __name__ == '__main__':
 
         if red_point[0] != 0:
             frame = cam.draw_point(frame, red_point)
-
+        
+        # 获取红点坐标
         r_x, r_y = red_point[0], red_point[1]
 
+        # 计算红点坐标和跟踪点的差值
         dx = r_x - x
         dy = r_y - y
 
-        x = int(x + dx*0.2)
-        y = int(y + dy*0.2)
+        # 计算新的跟踪点坐标
+        new_x = int(x + dx*0.2)
+        new_y = int(y + dy*0.2)
 
-        track_point = [x, y, 30, 30]
+        track_point = [new_x, new_x, 30, 30]
 
+        # 绘制跟踪点
         frame = cam.draw_point(frame, track_point)
 
         # 显示图像
