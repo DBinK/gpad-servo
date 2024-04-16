9ccbb5891ee73b1917730c89036cf5ae7f5dbede
DBinK <DBinKv1@gmail.com>
2024-04-16 10:38:59 +0000

    format🥚:

--- test.py
+++ test.py
@@ -5,15 +5,11 @@ servo = servo_driver.ServoController()
 
 top_angle = 90
 btn_angle = 90
-
+speed = 0.3
 on = 1
 
-
-
-
 def on_key_press(event):
-    global top_angle, btn_angle, on
-    speed = 0.5
+    global top_angle, btn_angle, speed, on
 
     if event.name == 'a':
         
@@ -41,6 +37,9 @@ def on_key_press(event):
 
     elif event.name == 'r':
         servo.reset()
+        top_angle = 90
+        btn_angle = 90
+
         print(f"重置位置")
 
     elif event.name == 'space':
