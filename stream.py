dae6b59659dfe68b0dc6675330ba94d5443ba1fa
DBinK <dbinkv1@gmail.com>
2024-04-17 22:58:11 +0800

    fix🐛:  更新ip

--- stream.py
+++ stream.py
@@ -299,7 +299,7 @@ if __name__ == '__main__':
             # 320x240 640x480 960x720 1280x720 1920x1080
             #url = 'http://192.168.100.44:4747/video?960x720'
             #url = 'rtsp://192.168.100.4:8080/video/h264'
-            url = 'http://192.168.226.252:8080/video/mjpeg'
+            url = 'http://192.168.89.205:8080/video/mjpeg'
             stream = ThreadedCamera(url)
 
             while True:
